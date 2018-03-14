be993c7aede03099f582552b5ba452883a9a6eb6
ckrueger6 <kitcatherinek@gmail.com>
2018-03-14 02:27:07 -0500

    kill js

--- Schedule/index.js
+++
@@ -1,4 +0,0 @@
-
-//the "sign up!"" buttons probably should change when they're clicked, and should probably in some way make the submit button more visible
-//what the fuck is a date picker
-$('.datepicker').datepicker([options])
